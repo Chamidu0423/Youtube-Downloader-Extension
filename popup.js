47b425629a8abf7feb71f6583fb138845963aea2
Chamidu Dilshan <chamidudilshan0423@gmail.com>
2025-12-04 06:05:27 +0530

    Enhance downloader options and UI in popup

--- popup.js
+++ popup.js
@@ -2,10 +2,19 @@ document.getElementById('downloadBtn').addEventListener('click', () => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     const currentUrl = tabs[0].url;
     const messageDiv = document.getElementById('message');
+    const service = document.getElementById('serviceSelect').value;
 
     if (currentUrl.includes("youtube.com/watch")) {
       
-      const newUrl = currentUrl.replace("youtube.com", "ssyoutube.com");
+      let newUrl = currentUrl;
+
+      if (service === "yout") {
+        newUrl = currentUrl.replace("youtube.com", "yout.com"); 
+      } else if (service === "ss") {
+        newUrl = currentUrl.replace("youtube.com", "ssyoutube.com");
+      } else if (service === "vd") {
+        newUrl = currentUrl.replace("youtube.com", "vdyoutube.com");
+      }
       
       chrome.tabs.create({ url: newUrl });
 
